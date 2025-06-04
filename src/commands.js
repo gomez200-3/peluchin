63f9c97585246f8f073189bc01f2b693c7cff958
gomez200-3 <gjhesid@gmail.com>
2025-06-04 16:44:48 -0500

    Update commands.js

--- src/commands.js
+++ src/commands.js
@@ -1,16 +1,18 @@
-import { chiste } from '.src/funciones/entretenimiento.js'
-import { menuPrincipal } from '.src/utils/helpers.js'
+const { chiste } = require('./funciones/entretenimiento.js');
+const { menuPrincipal } = require('./utils/helpers.js');
 
 // Mapea cada comando al módulo y función correspondiente
-export async function ejecutarComando(cmd, args, message) {
+async function ejecutarComando(cmd, args, message) {
     switch (cmd) {
         case 'chiste':
-            return await chiste()
+            return await chiste();
         case 'menu':
         case 'ayuda':
-            return menuPrincipal()
+            return menuPrincipal();
         // Agrega más comandos aquí...
         default:
-            return "Comando no reconocido. Usa /menu para ver las opciones."
+            return "Comando no reconocido. Usa /menu para ver las opciones.";
     }
 }
+
+module.exports = { ejecutarComando };
