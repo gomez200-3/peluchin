174865d875e4b0fe3c6de544dca22a4dc59cba0c
gomez200-3 <gjhesid@gmail.com>
2025-06-04 15:05:57 -0500

    Create helpers.js

---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+export function menuPrincipal() {
+    return `
+🌟 MENÚ PRINCIPAL 🌟
+
+/chiste - Pídele un chiste al bot
+/menu o /ayuda - Muestra este menú
+
+Próximamente:
+- Juegos, trivias, modo estudiantes, VIP y familiar.
+
+¡Comenta qué más te gustaría!
+`
+}
