43c8f154036fb0c2a74340df986e231112301626
gomez200-3 <gjhesid@gmail.com>
2025-06-04 15:01:43 -0500

    Create commands.js

---
+++ src/commands.js
@@ -0,0 +1,16 @@
+import { chiste } from './funciones/entretenimiento.js'
+import { menuPrincipal } from './utils/helpers.js'
+
+// Mapea cada comando al módulo y función correspondiente
+export async function ejecutarComando(cmd, args, message) {
+    switch (cmd) {
+        case 'chiste':
+            return await chiste()
+        case 'menu':
+        case 'ayuda':
+            return menuPrincipal()
+        // Agrega más comandos aquí...
+        default:
+            return "Comando no reconocido. Usa /menu para ver las opciones."
+    }
+}
